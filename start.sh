f7dcaf7701612fd8d4ea5fd8f90fcfff0d3e7920
janeffer945 <janeffernjeri945@gmail.com>
2022-05-17 12:47:09 +0300

    modified start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+export SECRET_KEY=555666777
+export MAIL_USERNAME=janeffernjeri945@gmail.com
+# export MAIL_PASSWORD=janeffernjerikinyua
+export MAIL_PASSWORD=Mbuguack7
+export DATABASE_URL='postgresql+psycopg2://moringa:Mbuguack@localhost/pitches'
+python3.8 manage.py server
+
